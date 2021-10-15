3c69cfeb9a30c683ead52fe3ff41a1ebd5d2af4e
Youssef-C <youssef.chardi@student.kyh.se>
2021-10-15 09:17:56 +0200

    styling started

--- react-firebase-cargo/src/components/GlobalStyles/globalStyles.js
+++ react-firebase-cargo/src/components/GlobalStyles/globalStyles.js
@@ -2,8 +2,8 @@ import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
     body{
-        background: teal;
-        color: white;
+        background: white;
+        padding: 20px;
     }
 
     button {
@@ -11,11 +11,23 @@ const GlobalStyle = createGlobalStyle`
         background-color: rgba(67, 69, 108, 1);
         border-radius: 32px;
         border: none;
-        position: absolute;
         width: 250px;
         height: 50px;
         left: 64px;
         top: 459px;
+    }
+
+    input {
+        font-family: 'Roboto', sans-serif;
+        font-size: 18px;
+        border: 2px solid rgba(67, 69, 108, 1);
+        border-radius: 32px;
+        padding: 2px 12px;
+        margin: 10px;
+        width: 300px;
+        height: 46px;
+        left: 38px;
+        top: 219px;
     }
     `;
 
